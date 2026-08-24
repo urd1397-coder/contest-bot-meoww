13911e5d40e5fb8576d023ce5ee4051891aec111
urd1397-coder <urd1397@gmail.com>
2026-08-24 03:35:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def cmd_developer(message):
 def cmd_buy(message):
     user_id = message.from_user.id
     if user_id in paid_users or user_id == OWNER_ID:
-        bot.reply_to(message, "😸 أنت تملك رخصة تفعيل البوت الفخرية (مجانية للمطور وبلاش)!")
+        bot.reply_to(message, "😸 أنت تملك رخصة تفعيل البوت الفخرية (مجانية للمطور)!")
         return
     prices = [LabeledPrice(label="رخصة تفعيل شركس للمسابقات", amount=50)]
     bot.send_invoice(
@@ -87,7 +87,7 @@ def got_payment(message):
 def start_contest(message):
     user_id = message.from_user.id
        # 👑 قائمة الأبطال المستثنين من الدفع (المطور الحالي + حساباتك الاحتياطية)
-    allowed_devs = [1488292943] # يمكنك إضافة آيدي حسابك الثاني هنا وبجانبه فاصلة ,
+    allowed_devs = [7963720007] # يمكنك إضافة آيدي حسابك الثاني هنا وبجانبه فاصلة ,
     
     if int(user_id) not in allowed_devs and user_id not in paid_users:
 
