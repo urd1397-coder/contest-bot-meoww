85a307305d8a9943fe22cea0b9504130c1ddbdc8
urd1397-coder <urd1397@gmail.com>
2026-08-29 00:40:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,13 +73,15 @@ def group_admin_mention_handler(message):
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_headers()
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
         self.wfile.write(b"Sharx Bot is active and running!")
 
     def do_HEAD(self):
         self.send_response(200)
-        self.send_headers()
-
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        
 def run_server():
     server = HTTPServer(('0.0.0.0', PORT), SimpleHandler)
     server.serve_forever()
